49f8f0af4182fcc51b1b3732c66b394fca31d807
nova <novaex@tuta.io>
2023-10-21 14:29:41 -0500

    remove bot environment

--- hand_of_light.py
+++ hand_of_light.py
@@ -7,7 +7,7 @@ import eien
 import eien_utils
 
 load_dotenv()
-TOKEN = os.getenv('TEST_TOKEN')
+TOKEN = os.getenv('PROD_TOKEN')
 
 bot = commands.Bot(command_prefix="#",intents=discord.Intents.all())
 
